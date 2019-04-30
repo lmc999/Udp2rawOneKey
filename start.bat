3281371fb7286ff7708c1494fbaef76cff96e869
lmc999 <39726914+lmc999@users.noreply.github.com>
2019-04-30 10:13:09 +0800

    Create start.bat

---
+++ start.bat
@@ -0,0 +1,5 @@
+@echo off
+cd /d %~dp0
+%1 start mshta vbscript:createobject("wscript.shell").run("""%~0"" ::",0)(window.close)&&exit
+
+start /b C:\bat\udp2raw.exe -c -r44.55.66.77:9898 -l 127.0.0.1:9999 --raw-mode faketcp -k passwd
